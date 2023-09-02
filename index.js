f45110f95e8709aa3ddf69a81d94bf47aa880609
nis06 <112106948+nis06@users.noreply.github.com>
2023-09-02 14:22:00 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const inputBox=document.getElementById("input-box");
+const list=document.getElementById("list-container");
+
+
+function addTask(){
+    if(inputBox.value===""){
+        alert("You must write something");
+    }
+    else{
+        let listData=document.createElement("li");
+        listData.innerText=inputBox.value;
+        list.appendChild(listData);
+
+        let span=document.createElement("span");
+        span.innerHTML="\u00d7";
+        listData.appendChild(span);
+
+    }
+   inputBox.value='';
+   saveData();
+}
+
+list.addEventListener('click',function(e){
+    if(e.target.tagName==='LI'){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName==="SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+},false );
+
+function saveData(){
+    localStorage.setItem("data",list.innerHTML);
+}
+
+function showTask(){
+    list.innerHTML=localStorage.getItem("data");
+}
+
+showTask();
